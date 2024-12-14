31738dc906d147f26f6ea6f7c33fa6f08f1670fd
Bobby Byrnes <rmbyrnes795@yahoo.com>
2024-12-14 02:06:22 -0500

    Add logger method descriptions

--- logger.js
+++ logger.js
@@ -1,11 +1,13 @@
+// Logs information messages
 function logMessage(message) {
     const timestamp = new Date().toISOString();
     console.log(`${timestamp}: ${message}`);
 };
 
+// Logs error messages
 function logError(message, error) {
     const timestamp = new Date().toISOString();
     console.error(`${timestamp}: ${message}`, error);
 };
 
-module.exports = {logMessage, logError};
+module.exports = { logMessage, logError };
